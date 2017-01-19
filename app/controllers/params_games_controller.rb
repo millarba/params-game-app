9c78b0d17a03151bbfad5abebc3297c996b59973
millarba <millarba@gmail.com>
2017-01-18 20:58:11 -0600

    form params

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -30,4 +30,36 @@ class ParamsGamesController < ApplicationController
       @response = "You got it!"
     end
   end
+
+  def get_form
+
+  end
+
+  def use_form
+    answer = 42
+    @value = params["form_message"].to_i
+    if @value < answer
+      @response = "Guess higher!"
+    elsif @value > answer
+      @response = "Guess lower!"
+    else
+      @response = "You got it!"
+    end
+  end
+
+  def get_guess
+
+  end
+
+  def use_guess
+    answer = 42
+    @guess = params["form_guess"].to_i
+    if @guess > answer
+      @response = "Guess lower"
+    elsif @guess < answer
+      @response = "Guess higher"
+    else
+      @response = "You got it!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get '/param_game_2' => 'params_games#second_game'
 
   get '/url_segment_params/:wildcard' => 'params_games#segment_params'
+
+  get '/form_params_url' => 'params_games#get_form'
+  post '/form_result_url' => 'params_games#use_form'
+
+  get '/guess_form_url' => 'params_games#get_guess'
+  post '/guess_result_url' => 'params_games#use_guess'
 end
